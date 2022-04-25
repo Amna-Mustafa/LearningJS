6533957e8ce582d514648b13a748ff246ec35512
Amna Mustafa <amnamustfa@MP15-AmnaM-SE-065.local>
2022-04-25 22:36:47 +0500

    Installed  Eslint

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,20 +1,19 @@
-console.log('Testing');
-const http = require('http');
+console.log("Testing");
+const http = require("http");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
- 
-var today=new Date();
+const today = new Date();
 console.log(today.getDate());
 console.log(today.getDay());
 console.log(today.getDay());
@@ -23,7 +22,6 @@ console.log(today.getHours());
 console.log(today.getMinutes());
 console.log(today.getSeconds());
 
-var st1 ="Hello,";
-var st2="How are you doing?";
-var st3="";
+const st1 = "Hello,";
+const st2 = "How are you doing?";
 console.log(st1 + st2);
